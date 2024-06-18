9bfbde12c0ee5edaf986c3c2fbdc71969d221b0a
akinoyor <155416793+akinoyor@users.noreply.github.com>
2024-06-18 08:36:01 +0000

    データ取得

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log('Hello');
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const ul = document.getElementById('ul');
+
+form.addEventListener('submit', function(event){
+  event.preventDefault();
+  console.log(input.value);
+  add();
+});
+
+function add(){
+  const li = document.createElement('il');
+  li.innerText = input.value;
+  li.classList.add('list-group-item')
+  ul.appendChild(li);
+  input.value ='';
+};
